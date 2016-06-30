68bb95c4f923898c29152ff82ab78a3a43b5b595
Yu-hsiang Huang <JillHuang@Yu-hsiangs-MacBook-Air.local>
2016-06-29 23:26:24 -0700

    change

--- Course.java
+++ Course.java
@@ -1,7 +1,35 @@
 
 public class Course {
-
-//--
-
-//jjj
+	
+    private int currentEnrollment;
+	private String courseName;	
+	private int maxNumStudents;
+	
+	public Course(String courseName, int maxNumStudents){
+		this.courseName = newcourseName;
+		this.maxNumStudents = newmaxNumStudents;
+		currentEnrollment =0;
+	}
+	
+	public int getCurrentEnrollment(){
+		return currentEnrollment;
+	}
+	public String getCourseName(){
+		return courseName;
+	}
+	public int getMaxNumStudents(){
+		return maxNumStudents;
+	}
+	public void setCourseName(String courseName){
+		this.courseName = newcourseName;;
+	}
+	public void setMaxNumStudents(int maxNumStudents){
+		this.maxNumStudents = newmaxNumStudents;
+	}
+	
+	public String toString(){
+		String s = courseName +", "+ maxNumStudents;
+		return s;
+	}
+	
 }
